85898b7caa33467097f1e0cde56776b8783fe3d9
Myrella Gomes <myrellagomes832@gmail.com>
2026-07-29 18:34:09 -0300

    feat: update Footer component content and layout for improved presentation

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
 export function Footer() {
   return (
-    <footer className="mt-20 border-t flex flex-col items-center">
+    <footer className="mt-20 border-t flex flex-col items-center gap-4 justify-end">
       <div>
-        © 2026 STATIC//NOISE. all rights reversed. this blog it's just a
-        agressive feeling, like a rock song.
+        © 2026 my little--noise. all rights reversed. 
+      </div>
+      <p>this blog it's just a
+        agressive feeling, like a nu metal song.</p>
+      <div className="blink text-accent">
+        {`>`} connection ended
       </div>
     </footer>
   );
